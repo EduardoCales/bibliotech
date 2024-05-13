0efaeba78e27197bf72990d273a983fcd0706bb1
Eduardo Cales <dudu.calesferreira@gmail.com>
2024-05-13 13:38:34 -0300

    super usuario criado/ apaguei td oq tinha dentro pq vou ter q fazer dnv/ tem q configurar o bootstrap e o js e ja tem algumas coisas cadastradas

--- biblioteca/admin.py
+++ biblioteca/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Category, Livro
 
-# Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    ...
+
+@admin.register(Livro)
+class LivroAdmin(admin.ModelAdmin):
+    ...
+
+admin.site.register(Category, CategoryAdmin)
--- biblioteca/models.py
+++ biblioteca/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Category(models.Model):
+     name = models.CharField(max_length=65)
+
+     def __str__(self):
+        return self.name
+
+class Livro(models.Model):
+     title = models.CharField(max_length=65)
+     description = models.CharField(max_length=165)
+     created_ad = models.DateTimeField(auto_now_add=True)
+     is_published = models.BooleanField(default=False)
+     covers = models.ImageField(upload_to='biblioteca/covers/%Y/%m/%d/')
+     category = models.ForeignKey(
+          Category, on_delete=models.SET_NULL, null=True
+          )
+     author = models.ForeignKey(
+          User, on_delete=models.SET_NULL, null=True
+          )
+     
+     def __str__(self):
+         return self.title
