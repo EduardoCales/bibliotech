383202f34f57f219220cdd62aef8e9065493c0a1
Eduardo Cales <dudu.calesferreira@gmail.com>
2024-05-13 02:39:06 -0300

    arquivos estaticos

--- biblioteca/views.py
+++ biblioteca/views.py
@@ -2,10 +2,10 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def login(request):
-    return render(request, 'login.html')
+    return render(request, 'bibliotech/login.html')
 
 def cadastro(request):
-    return render(request, 'cadastro.html')
+    return render(request, 'bibliotech/cadastro.html')
 
 def home(request):
     return HttpResponse('home')
--- projeto/settings.py
+++ projeto/settings.py
@@ -56,7 +56,10 @@ ROOT_URLCONF = 'projeto.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join((BASE_DIR), 'templates')],
+        'DIRS': [
+            BASE_DIR / 'base_templates', 
+            os.path.join((BASE_DIR), 'templates'),
+            ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
