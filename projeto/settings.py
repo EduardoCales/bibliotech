ee5381c2db43f99db2c2ae5c36146b1ebd863c91
Eduardo Cales <dudu.calesferreira@gmail.com>
2024-05-13 12:08:38 -0300

    dia 13

--- projeto/settings.py
+++ projeto/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
-import os
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -57,8 +57,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            BASE_DIR / 'base_templates', 
-            os.path.join((BASE_DIR), 'templates'),
+            BASE_DIR / 'base_templates',
             ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -120,9 +119,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = 'static'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_URL = '/static/'
+STATICFILES_DIRS =  [
+    BASE_DIR / 'base_static'
+]
+
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
