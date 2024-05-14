3992babf30c87e2f01e5aee64303c3b31eb40f84
Eduardo Cales <dudu.calesferreira@gmail.com>
2024-05-13 21:58:13 -0300

    ultima att antes de mandar pros mlk

--- biblioteca/urls.py
+++ biblioteca/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from biblioteca.views import login, cadastro, home
+from . import views
 
 
 urlpatterns = [
-    path('', login),
-    path('cadastro/', cadastro),
-    path('home/', home),
+    path('', views.login),
+    path('cadastro/', views.cadastro),
 ]
--- biblioteca/views.py
+++ biblioteca/views.py
@@ -2,10 +2,8 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def login(request):
-    return render(request, 'bibliotech/login.html')
+    return render(request, 'bibliotech/pages/login.html')
 
 def cadastro(request):
-    return render(request, 'bibliotech/cadastro.html')
+    return render(request, 'bibliotech/pages/cadastro.html')
 
-def home(request):
-    return HttpResponse('home')
